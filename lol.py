f8a85753717d79fe52108bba0a2e4723123b24a1
ffdvl1120 <91732122+ffdvl1120@users.noreply.github.com>
2022-09-27 13:23:13 +0500

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,1 +1,1 @@
-import marshal\nimport dis\nmagic = b'a\r\r\n\x00\x00\x00\x00\xf6\x971a\x00\x00\x00\x00'
+import marshal\nimport dis\nmagic = b'a\r\r\n\x00\x00\x00\x00\xf6\x971a\x00\x00\x00\x00'\n[::-1]))))\nwith open('test.pyc', 'wb') as pyc:\npyc.write(magic)\nmarshal.dump(dis.Bytecode(x).codeobj, pyc)
